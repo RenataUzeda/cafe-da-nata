e655a83ea114c1c5206d95b312410bc900e31a3e
RenataUzeda <renatauzedasilva@gmail.com>
2024-09-29 00:16:47 -0300

    feat: recria projeto Café da Nata.
    Recria projeto e adciona seção "Seu Pedido"e  estilizada-a.
    
    Autora: Renata Uzêda.

---
+++ script.js
@@ -0,0 +1,42 @@
+const navbar = document.querySelector(".navbar, .btn-home").offsetTop + 40;
+
+document.querySelectorAll(".navbar a, .btn-home").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); 
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    
+    let targetPosition = targetElement.offsetTop - navbar;
+
+    if (window.innerWidth <= 768) { 
+      targetPosition = targetElement.offsetTop - (navbar + 25);
+    }
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth", 
+    });
+  });
+});
+
+const menuButton = document.querySelector(".menu-resp");
+const closeButton = document.querySelector(".fechar-resp");
+const navbarMobile = document.querySelector(".navbar");
+
+menuButton.addEventListener("click", () => {
+  navbarMobile.classList.add("active"); 
+});
+
+
+closeButton.addEventListener("click", () => {
+  navbarMobile.classList.remove("active"); 
+});
+
+
+document.querySelectorAll(".navbar a").forEach((link) => {
+  link.addEventListener("click", function () {
+    navbarMobile.classList.remove("active"); 
+  });
+});
+
